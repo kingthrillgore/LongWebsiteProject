f9fc68c2f5eccf43980ac1d78456bc4b60ded51f
Cameron Kilgore <ghostfreeman@gmail.com>
2023-09-24 21:09:53 -0500

    Added code

---
+++ app/public/index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Long PHP Project</title>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+  </head>
+
+  <body>
+    <div id="parent" class="main slideout-panel slideout-panel-left">
+      <!-- Top -->
+      <div id="topnav">
+        <div class="container">
+          <div class="row">
+
+          </div>
+        </div>
+      </div>
+
+      <!-- Nav -->
+      <div class="row" id="nav">
+        <div class="col-1">
+          <!-- Home Link -->
+        </div>
+        <div class="col-11">
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+                <a href="#">Inventory</a>
+                <a href="#">Shop From Home</a>
+                <a href="#">About Us</a>
+                <a href="#">Finance</a>
+                <a href="#">News &amp; Updates</a>
+                <a href="#">Parts &amp; Service</a>
+                <a href="#">Long Bodyshop</a>
+                <a href="#">Login Here</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <!-- Content Start -->
+      <div class="main" id="main">
+        <div class="container-fluid">
+
+        </div>
+      </div>
+
+    </div>
+    
+    <h1>Hello, world! fgsfds</h1>
+  </body>
+
+</html>
